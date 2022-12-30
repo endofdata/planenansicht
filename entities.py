96dcef0b60ed410af85c91107cda9df78115178a
endofdata <endofdata@hotmail.de>
2022-12-30 17:29:27 +0100

    move selection to separate source, add some docs

--- entities.py
+++ entities.py
@@ -1,20 +1,32 @@
-from multiprocessing.sharedctypes import Value
 from bcrypt import hashpw, gensalt, checkpw
 from base64  import b64encode
 from hashlib import sha256
 
 class Damage:
+	"""Defines a certain kind of a damage, like 'Knopf fehlt' or 'Riss'
+	
+	This is the entity type for data rows in the Damages table of the Tarps database.
+	If multiple tarps have comparable issues, they can share the same Damage definition
+	"""
 	def __init__(self, id, code, description):
 		self.id = id
 		self.code = code
 		self.description = description
 
 class TarpType:
+	"""Defines a tarp type i.e., 'Kotenplane' or 'Jurtenopi'
+	
+	This is the entity type for data rows in the TarpTypes table of the Tarps database.
+	"""
 	def __init__(self, id, name):
 		self.id = id
 		self.name = name
 
 class TarpCategory:
+	"""Defines a tarp category e.g., the 'A', 'B' or 'C' category of tarp type 'Kotenplane'
+
+	This is the entity type for data rows in the Categories table of the Tarps database.
+	"""
 	def __init__(self, id, name, width, length, additional, tarp_type):
 		self.id = id
 		self.name = name
@@ -24,6 +36,10 @@ class TarpCategory:
 		self.tarp_type = tarp_type
 
 class Tarp:
+	"""Defines a tarp including annotation, category and damages
+
+	This is the entity type for data rows in the Tarps table of the Tarps database.
+	"""
 	def __init__(self, id, number, annotation, category, damages):
 		self.id = id
 		self.number = number
@@ -31,92 +47,12 @@ class Tarp:
 		self.category = category
 		self.damages = damages
 
-class Selector:
-	def __init__(self, property = None, values = None, is_pattern = False):
-		self.property = property
-		self.values = values
-		self.is_pattern = is_pattern
-
-	def to_sql(self):
-		predicate = ""
-
-		if self.property != None:
-			if len(self.values) > 0:
-				for value in self.values:
-					if self.is_pattern:
-						predicate += f"({self.property} LIKE '%{value}%') OR "
-					else:
-						predicate += f"({self.property} = '{value}') OR "
-
-				# strip trailing "OR "
-				predicate = predicate[0:-3]
-			else:
-				predicate = f"{self.property} IS NULL"
-
-		return predicate
-
-class Order:
-	def __init__(self, property = None, is_descending = False):
-		self.property = property
-		self.is_descending = is_descending
-
-	def to_sql(self):
-		sql = ""
-
-		if self.property != None:
-			if self.is_descending:
-				dir = "DESC"
-			else:
-				dir = "ASC"
-
-			sql = f"{self.property} {dir}"
-		
-		return sql
-
-class Selection:
-	def __init__(self, selectors, sequence):
-		self.selectors = selectors
-		self.sequence = sequence
-
-	def has_selectors(self):
-		for selector in self.selectors:
-			if selector.property != None:
-				return True
-
-		return False
-
-	def has_sequence(self):
-		for order in self.sequence:
-			if order.property != None:
-				return True
-
-		return False
-
-	def to_sql(self):
-		predicate = ""
-
-		if self.has_selectors():
-			predicate += " WHERE "
-
-			for selector in self.selectors:
-				if selector.property != None:
-					predicate += f"({selector.to_sql()}) AND "
-			
-			# strip trailing 'AND '
-			predicate = predicate[0:-4]
-
-		if self.has_sequence():
-			predicate += " ORDER BY "
-			for order in self.sequence:
-				if order.property != None:
-					predicate += f"{order.to_sql()}, "
-
-			#strip trailin ', '
-			predicate = predicate[0:-2]
-
-		return predicate
 
 class User:
+	"""Defines a user who is allowed to work on the Tarps database
+
+	This is the entity type for data rows in the Users table of the Users database.
+	"""
 	def __init__(self, id, name, display, pwd_hash, rights):
 		self.id = id
 		self.name = name
@@ -125,17 +61,43 @@ class User:
 		self.rights = rights
 
 	def encrypt_pwd(self, pwd):
+		"""Updates the pwd_hash based on the specified pwd
+
+		Generates a hashed password using the base-64 encoded SHA-256 of the specified pwd and stores
+		it in self.pwd_hash.
+
+		Args:
+			pwd: new password as entered by the user
+
+		Returns:
+			The new value of pwd_hash
+		"""
 		if pwd == None or pwd == "":
 			raise ValueError("The password cannot be empty or unset")
 		self.pwd_hash = hashpw(b64encode(sha256(pwd.encode()).digest()), gensalt()).decode()
 		return self.pwd_hash
 		
 	def check_pwd(self, pwd):
+		"""Validates the specified pwd based on the previously stored pwd_hash
+
+			Checks a hashed password generated from the base-64 encoded SHA-256 of the specified pwd
+			against the previously stored pwd_hash.
+
+		Args:
+			pwd: password as entered by the user
+
+		Returns:
+			True if the validation was successful e.g., the pwd is correct. Otherwise false.
+		"""
 		if pwd == None or pwd == "":
 			return False
 		return checkpw(b64encode(sha256(pwd.encode()).digest()), self.pwd_hash.encode())
 
 class AccessRight:
+	"""Defines a permission for a certain program function
+
+	This is the entity type for data rows in the AccessRights table of the Users database.
+	"""
 	def __init__(self, id, display):
 		self.id = id
 		self.display = display
---
+++ selection.py
@@ -0,0 +1,152 @@
+#from multiprocessing.sharedctypes import Value
+
+class Selector:
+	"""Describes a property and its values in a Selection"""
+	def __init__(self, property = None, values = None, is_pattern = False):
+		self.property = property
+		self.values = values
+		self.is_pattern = is_pattern
+
+	def to_sql(self):
+		"""Creates a partial SQL statement that can be used in a WHERE clause
+		
+		The generated SQL is a logical 'OR' combination comparing the property with each
+		of the specified values. The is_pattern flag controls, whether a string pattern
+		comparison or a direct value comparison is generated. If no values are specified,
+		the generated SQL checks the property to be NULL. If no property name is specified,
+		the result is an empty string.
+
+		Examples:
+
+		If property is set to 'category' and values are 'A' and 'D', the resulting SQL is:
+
+		(category = 'A') OR (category = 'D')
+
+		If property is set to 'annotation', values are 'rostig' and 'an der Kante' and 
+		is_pattern is set to True, the resulting SQL is:
+
+		(annotation LIKE '%rostig%') OR (annotation LIKE '%an der Kante%')
+
+		If property is set to 'number' and no values are specified, the resulting SQL is:
+
+		number IS NULL
+		"""
+		predicate = ""
+
+		if self.property != None:
+			if len(self.values) > 0:
+				for value in self.values:
+					if self.is_pattern:
+						predicate += f"({self.property} LIKE '%{value}%') OR "
+					else:
+						predicate += f"({self.property} = '{value}') OR "
+
+				# strip trailing "OR "
+				predicate = predicate[0:-3]
+			else:
+				predicate = f"{self.property} IS NULL"
+
+		return predicate
+
+class Order:
+	"""Describes the sorting directory for an SQL result"""
+	def __init__(self, property = None, is_descending = False):
+		self.property = property
+		self.is_descending = is_descending
+
+	"""Creates a partial SQL statement that can be used in an ORDER BY clause.
+
+	The resulting SQL is the property name followed b y the SQL code for the sort direction,
+	which is DESC when is_descending is set to True or ASC otherwise. If no property name is
+	specified, the result is an empty string.
+
+	Example:
+
+	If property is set to 'annotation' and is_descending is set to False, the resulting SQL is:
+
+	annotation ASC
+	"""
+	def to_sql(self):
+		sql = ""
+
+		if self.property != None:
+			if self.is_descending:
+				dir = "DESC"
+			else:
+				dir = "ASC"
+
+			sql = f"{self.property} {dir}"
+		
+		return sql
+
+class Selection:
+	"""Defines a database selection based on property selectors and sorting sequence
+
+	The selection can be formatted as partial SQL statement by calling the to_sql() method.
+	"""
+	def __init__(self, selectors, sequence):
+		"""Constructor
+
+		Args:
+			selectors: a list of zero or more Selector objects that define the filter criteria.
+			sequence: a list of zero or more Order objects that define the sorting order of the result.
+		"""
+		self.selectors = selectors
+		self.sequence = sequence
+
+	def has_selectors(self):
+		"""Checks if the selectors list contains valid entries
+
+		Returns:
+			True, if there is at least one selector with a specified property name. False otherwise.
+		"""
+		for selector in self.selectors:
+			if selector.property != None:
+				return True
+
+		return False
+
+	def has_sequence(self):
+		"""Checks if the sequence list contains valid entries
+		
+		Returns:
+			True, if there is at least one entry in the sorting sequence list with a specified property name. False otherwise.
+		"""
+		for order in self.sequence:
+			if order.property != None:
+				return True
+
+		return False
+
+	def to_sql(self):
+		"""Creates a partial SQL statement with optional WHERE and ORDER BY clauses
+
+		If the selection has selectors, these are combined with logical 'AND' to form a WHERE clause. 
+		
+		If the selection has sequence items, these are listed comma-separated to form an ORDER BY clause.
+
+		If neither selectors nor sequence items are defined, the result is an empty string. Otherwise, it
+		either starts with ' WHERE ' or ' ORDER BY ' and can be appended to a SELECT statement.
+		"""
+		predicate = ""
+
+		if self.has_selectors():
+			predicate += " WHERE "
+
+			for selector in self.selectors:
+				if selector.property != None:
+					predicate += f"({selector.to_sql()}) AND "
+			
+			# strip trailing 'AND '
+			predicate = predicate[0:-4]
+
+		if self.has_sequence():
+			predicate += " ORDER BY "
+			for order in self.sequence:
+				if order.property != None:
+					predicate += f"{order.to_sql()}, "
+
+			#strip trailin ', '
+			predicate = predicate[0:-2]
+
+		return predicate
